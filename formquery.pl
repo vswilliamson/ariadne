8ea63888512235315e7a1ba090e485cd2348001c
Vernell Williamson <vswilliamson@vcu.edu>
2015-04-10 12:14:33 -0400

    update formquery.pl

--- formquery.pl
+++ formquery.pl
@@ -5,4 +5,17 @@
 #contact VS Williamson, PHd.
 #Vernell.Williamson@vcuhealth.org
 
-use CGIqw (:
+use strict;
+use warnings; 
+use CGI;
+use CGI: Carp qw (fatals to Browser)
+print header; 
+print start_html ("Search Results")
+print h2 ("Your Query")
+my %form;
+foreach my $p (param()){
+  $form{$p} = param($p);
+#insert results here:
+  print "$p = $form{$p}<br>\n";
+  
+  }
