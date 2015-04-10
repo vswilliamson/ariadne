c0902f2b7ebc89f82817e9cfecb05e85da2e3414
Vernell Williamson <vswilliamson@vcu.edu>
2015-04-10 12:06:21 -0400

    create formquery.pl

---
+++ formquery.pl
@@ -0,0 +1,8 @@
+#!/usr/bin/perl
+# this script acts as an interface with the desk ico to either query known databases or
+#to run specific programs to determine individual SNP function
+# problems?
+#contact VS Williamson, PHd.
+#Vernell.Williamson@vcuhealth.org
+
+use CGIqw (:
