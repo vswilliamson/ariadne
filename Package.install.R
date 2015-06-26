27948c4239d25c4e4698e8ae7fb48db60776499d
Vernell Williamson <vswilliamson@users.noreply.github.com>
2015-06-26 16:35:36 -0400

    Create Package.install.R

---
+++ Package.install.R
@@ -0,0 +1,13 @@
+#install.packages.R
+# to loadin packages
+source("http://bioconductor.org/biocLite.R")
+biocLite("rfPred") # installs protein package
+install.packages("gdata")
+install.packages("gridExtra")
+biocLite(rtracklayer)
+install.packages(stringr)
+biocLite(SomaticCancerAlterations)
+biocLite(GenomicRanges)
+install.packages(ggbio)
+install.packages(data.table)
+install.packages(plyr)
