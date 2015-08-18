2abb228398dc494cc22e2c3f7b56711940fcf90a
Vernell Williamson <vswilliamson@users.noreply.github.com>
2015-08-18 10:53:50 -0400

    update update_data.pl

--- update_data.pl
+++ update_data.pl
@@ -1,1 +1,16 @@
 #this script queries available databases for updates
+use strict; 
+use warnings;
+# test if file has been accessed in last 30 days. 
+my $directory = '/tmp';
+opendir (DIR, $directory) or die $!;
+if (-A $filename > 30) {
+  print "$filename has not been accessed in at least 30 days\n";
+}
+
+
+while (my $file = readdir(DIR)) {
+next if ($file =~ m/^\./);
+print "$file\n";
+}
+closedir(DIR);
